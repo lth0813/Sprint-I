3b334936d7979c607b80bd88dacb617d5df321a0
lth0813 <143094045+lth0813@users.noreply.github.com>
2023-12-07 18:57:17 +0900

    Delete src/main/java/com/example/sprint1 directory

--- src/main/java/com/example/sprint1/Controller/DBController.java
+++
@@ -1,9 +0,0 @@
-package com.example.sprint1.Controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class DBController {
-    @Autowired
-}
