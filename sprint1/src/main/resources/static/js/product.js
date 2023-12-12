77b472fe35138677d0f8dfe4f401776595fe20a9
whrkdud <whrkdud52@naver.com>
2023-12-12 23:23:24 +0900

    main, product - html,css,js 수정

--- sprint1/src/main/resources/static/js/product.js
+++ sprint1/src/main/resources/static/js/product.js
@@ -1,18 +1,16 @@
+// 주문 금액 가져오기
+const priceElement = document.querySelector('.cartListDetail .price');
+const orderAmount = priceElement.textContent.trim(); // 주문금액 텍스트를 가져옴
+
+// 버튼 업데이트
+const orderButton = document.querySelector('.cartBigorderbtn');
+orderButton.textContent = `총 ${orderAmount} 결제하기`;
+
 // 로고를 누르면 main으로 이동 //
 const logo = document.querySelector("#logo > img");
 logo.addEventListener("click", function() {
     location.href="http://localhost/main";
 });
-
-// 광고 //
-const sliderInner = document.querySelector(".sliderInner");
-const slider = document.querySelectorAll(".slider");
-let currrentIndex = 0;
-sliderInner.style.transition = "all 1.6s";
-setInterval(()=>{
-    currrentIndex = (currrentIndex + 1) % slider.length;
-    sliderInner.style.transform = "translateX(" + -1280 * currrentIndex + "px)";
-}, 15000);
 
 // 상세검색을 누르면 상세검색 모달창 띄우기 //
 const modal = document.querySelector("#checkBox");
@@ -30,28 +28,6 @@ close.addEventListener("click", function() {
     blur.style.opacity = "1";
 });
 
-// 인기 상품을 클릭하면 해당 상품 상세 페이지로 이동 //
-const kumho = document.querySelector("#kumho");
-const nexen = document.querySelector("#nexen");
-const hankook= document.querySelector("#hankook");
-const continental = document.querySelector("#continental");
-const michelin = document.querySelector("#michelin");
-kumho.addEventListener("click", function() {
-    location.href = "/";
-});
-nexen.addEventListener("click", function() {
-    location.href = "/";
-});
-hankook.addEventListener("click", function() {
-    location.href = "/";
-});
-continental.addEventListener("click", function() {
-    location.href = "/";
-});
-michelin.addEventListener("click", function() {
-    location.href = "/";
-});
-
 // 로그인, 장바구니, 마이페이지를 클릭하면 해당 페이지로 이동 //
 const login = document.querySelector("#login");
 const cart = document.querySelector("#cart");
@@ -65,5 +41,3 @@ cart.addEventListener("click", function() {
 mypage.addEventListener("click", function() {
     location.href = "/";
 });
-
-// 인기 상품 hover시 상품명 띄우기 //
