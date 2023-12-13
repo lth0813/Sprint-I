b172f4e26f137f058e7f7bc8a46d802e7ffdd4f8
damhyang <150633152+damhyang@users.noreply.github.com>
2023-12-13 18:05:15 +0900

    20231213 KDH updatd cart.html,css ->create alert
    20231213 KDH updatd cart.html,css ->create alert

--- sprint1/src/main/resources/static/js/mypage.js
+++ sprint1/src/main/resources/static/js/mypage.js
@@ -1,5 +1,18 @@
 $(document).ready(function($) {
-    $(".scroll_move").click(function(event){ console.log(".scroll_move"); event.preventDefault();
+        $(".scroll_move").click(function(event){ console.log(".scroll_move"); 
+            event.preventDefault();
+        });
+        $('html, body').animate({scrollTop:$(this.hash).offset().bottom - 60}, 100);
     });
-    $('html, body').animate({scrollTop:$(this.hash).offset().top-500}, 1000);
-    });
+
+
+function modifybtn(){
+    alert('회원정보가 정상적으로 수정되었습니다.');
+};
+
+
+function deletebtn(){
+    alert('리뷰가 정상적으로 삭제되었습니다.');
+}
+
+
