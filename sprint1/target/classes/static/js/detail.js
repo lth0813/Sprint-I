7d4ddb3c05f19c37eada9976049810d869fb38d5
lth0813 <lth0813@gmail.com>
2023-12-16 17:57:52 +0900

    merging backup

--- sprint1/target/classes/static/js/detail.js
+++ sprint1/target/classes/static/js/detail.js
@@ -30,26 +30,115 @@ if (login != null) {
         location.href = "/login";
     });
 }
-cart.addEventListener("click", function() {
+cart.addEventListener("click", function(e) {
     if(login != null) {
         window.alert("로그인 후 이용해주세요.");
+        e.preventDefault();
     } else {
-        const id = document.querySelector("#userid").value;
+        const id = document.querySelector(".userid").value;
         location.href = "/cart?id=" + id;
     }
 });
-mypage.addEventListener("click", function() {
+mypage.addEventListener("click", function(e) {
     if(login != null) {
         window.alert("로그인 후 이용해주세요.");
+        e.preventDefault();
     } else {
-        const id = document.querySelector("#userid").value;
+        const id = document.querySelector(".userid").value;
         location.href = "/mypage?id=" + id;    
     }
 });
 
 // 로그아웃 //
 const logout = document.querySelector("#logout");
-logout.addEventListener("click", function() {
-    const form = document.forms.logout;
-    form.submit();
+if(logout != null) {
+    logout.addEventListener("click", function() {
+        const form = document.forms.logout;
+        form.submit();
 });
+}
+
+// 수량 선택 안하고 장바구니버튼, 구매하기버튼 클릭시 수량선택요청alert 띄우기 //
+// 장바구니버튼을 클릭하면 장바구니에 담기 완료 모달 띄우기 //
+// 로그인 안하고 장바구니버튼, 구매하기버튼 클릭시 로그인요청alert 띄우기 //
+const minus = document.querySelector(".qtyminus");
+    const plus = document.querySelector(".qtyplus");
+    const qty = document.querySelector(".qty");
+    const product_price = parseInt(document.querySelector(".product_price").innerHTML.replace(/,/g,''))
+    const product_qty = document.querySelectorAll(".product_qty");
+    const price = document.querySelector(".price")
+    minus.addEventListener("click",() => {
+        if (parseInt(qty.innerHTML) == 0) {
+            qty.innerHTML = 0;
+            for (let i = 0; i < product_qty.length; i++) {
+                product_qty[i].value = qty.innerHTML;
+            }  
+        }
+        else {
+            qty.innerHTML -= 1;
+            price.innerHTML = parseInt(qty.innerHTML) * product_price;
+            for (let i = 0; i < product_qty.length; i++) {
+                product_qty[i].value = qty.innerHTML;
+            } 
+        }
+    })
+    plus.addEventListener("click",() => {
+        qty.innerHTML = parseInt(qty.innerHTML) +1;
+        price.innerHTML = parseInt(qty.innerHTML) * product_price;
+        for (let i = 0; i < product_qty.length; i++) {
+            product_qty[i].value = qty.innerHTML;
+        } 
+    })
+    const cartinsert = document.querySelector(".cart");
+    const cartBtnLoginX = document.querySelector(".userid");
+    cartinsert.addEventListener("click",(e) => {
+        if(logout != null) {
+            if(parseInt(qty.innerHTML)<1){
+                alert("수량을 선택해주세요");
+                e.preventDefault();
+            } else {
+                // cartO.style.display = "block";
+                return false;
+            }
+        } else {
+            window.alert("로그인 후 이용해주세요.");
+            e.preventDefault();
+        }
+    });
+
+    const cartO = document.querySelector("#cartO");
+    const checkmodal = document.querySelector(".modal");
+    if (checkmodal != null) {
+        if (checkmodal.innerHTML == "O") {
+        cartO.style.display = "block";
+        blur.style.opacity = "0.5";
+    }}
+
+    const orderinsert = document.querySelector(".purchase");
+    orderinsert.addEventListener("click",(e) => {
+        if(logout != null) {
+            if(parseInt(qty.innerHTML)<1){
+                alert("수량을 선택해주세요");
+                e.preventDefault();
+            }
+        } else {
+            window.alert("로그인 후 이용해주세요.");
+            e.preventDefault();
+        }
+    });
+
+// 장바구니에 담기 완료 모달 닫기 //
+const cartOX = document.querySelector("#cartOX > p");
+if (cartOX != null) {
+    cartOX.addEventListener("click", function() {
+        cartO.style.display = "none";
+        blur.style.opacity = "1";
+    });
+}
+// 장바구니 모달 총 금액 표시 //
+const cartprice = parseInt(document.querySelector(".cartprice").innerHTML.replace(/,/g,''))
+console.log(cartprice)
+const cartqty = document.querySelector(".cartqty");
+console.log(cartqty.innerHTML)
+const carttotalprice = document.querySelector(".carttotalprice")
+carttotalprice.innerHTML = cartprice * parseInt(cartqty.innerHTML)
